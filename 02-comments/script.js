92e4e5419341fc916a53dadc61c3c5eba41e4bda
George Ifrim <ifrim.george.alexandru@gmail.com>
2024-06-02 13:45:24 +0300

    first commit

---
+++ 02-comments/script.js
@@ -0,0 +1,21 @@
+console.log(100);
+
+console.log('Buna dimineata')
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert')
+console.warn('Warning')
+console.table({name: 'Brad', email: 'brad@gmail.com'})
+
+console.group('simple')
+console.error('Alert')
+console.warn('Warning')
+console.table({name: 'Brad', email: 'brad@gmail.com'})
+console.groupEnd()
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%c Hello World!', styles);
